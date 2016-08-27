a063dca294876b8482bbb4a2daca5168d277a07c
edek99 <adrianczudej@gmail.com>
2016-08-27 19:32:31 +0200

    Update and rename README.md to README.m

---
+++ README.m
@@ -0,0 +1,3 @@
+# hello-world
+Hi Guys!
+Starting my adventure with front end with learning html,css,js and practice by making/editing projects and eventually becoming a front-end developer. Cheers~!
